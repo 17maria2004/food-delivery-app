b48efc18f644bac96e26a86c559ea15c659a6af9
Maria Choufani <mm4654801@gmail.com>
2025-07-24 11:37:00 +0300

    trigger redeploy

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const bcrypt = require("bcryptjs");
 dotenv.config();
 const app = express();
 
+//redeploy
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
